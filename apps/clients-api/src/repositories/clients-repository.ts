98753a43b27d9f3e8d6c3c904947cd14dafad42c
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-04-09 09:21:37 -0300

    feat: create clients repository method to get last saved account

--- apps/clients-api/src/repositories/clients-repository.ts
+++ apps/clients-api/src/repositories/clients-repository.ts
@@ -1,10 +1,11 @@
-import { eq } from 'drizzle-orm';
+import { desc, eq } from 'drizzle-orm';
 import { db } from '../database/connection';
 import { clients } from '../database/schema';
-import { Client } from '../models/client';
+import { BankingDetails, Client } from '../models/client';
 
 export interface ClientsRepository {
   getByEmail(email: string): Promise<Client | null>
+  getLastAccount(): Promise<BankingDetails | null>
   save(client: Client): Promise<{id: string}>
 }
 
@@ -13,14 +14,39 @@ export class ClientsRepositoryDatabase implements ClientsRepository {
   constructor() {}
 
   async getByEmail(email: string): Promise<Client | null> {
-    const client = await db.select().from(clients).where(eq(clients.email, email));
+    const data = await db.select().from(clients).where(eq(clients.email, email));
 
-    if (!client[0]) return null;
+    if (!data[0]) return null;
 
     return {
-      name: (client as unknown as Client).name,
-      email: (client as unknown as Client).email,
-      address: (client as unknown as Client).address,
+      name: data[0].name,
+      email: data[0].email,
+      address: data[0].address,
+      bankingDetails: {
+        agency: data[0].agency,
+        account: data[0].account,
+        digit: data[0].digit,
+      }
+    };
+  }
+
+  async getLastAccount(): Promise<BankingDetails | null> {
+    const data = await db
+      .select({
+        agency: clients.agency,
+        account: clients.account,
+        digit: clients.digit,
+      })
+      .from(clients)
+      .orderBy(desc(clients.createdAt))
+      .limit(1);
+
+    if(!data[0]) return null;
+
+    return {
+      agency: data[0].agency,
+      account: data[0].account,
+      digit: data[0].digit
     };
   }
 
@@ -29,6 +55,9 @@ export class ClientsRepositoryDatabase implements ClientsRepository {
       name: client.name,
       email: client.email,
       address: client.address,
+      agency: client.bankingDetails!.agency,
+      account: client.bankingDetails!.account,
+      digit: client.bankingDetails!.digit,
     }).returning();
 
     return {
